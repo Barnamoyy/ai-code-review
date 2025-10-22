9bfce29660ad92fb99b16f06f7a9de53245c43f6
HalkatHaathi <75882515+Barnamoyy@users.noreply.github.com>
2025-10-22 11:50:07 +0530

    changes

--- src/utils/pineconeClient.js
+++ src/utils/pineconeClient.js
@@ -1,5 +1,7 @@
 import { Pinecone } from '@pinecone-database/pinecone';
+import dotenv from "dotenv"
+dotenv.config(); 
 
 export const pinecone = new Pinecone({
-  apiKey: `pcsk_4vvv9i_E9aZfWH6WV4tYu5FhByhATtH7QSYqFiiU8tvA6HEHjRW3hjocmeJSQxN5jPWETK`
+  apiKey: process.env.PINECONE_API_KEY
 });
